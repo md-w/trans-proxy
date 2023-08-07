6db694251278fcb28446bc2302cac0a0be6ac003
Monotosh Das <monotosh.das@videonetics.com>
2023-08-07 14:21:52 +0000

    wip

--- proxy/main.go
+++ proxy/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/md-w/trans-proxy/proxy/data"
+	pb "github.com/md-w/trans-proxy/proxy/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
 
 func main() {
-	fmt.Printf("Started\n")
+	log.Printf("failed to listen: %v", "Starting")
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			*certFile = data.Path("x509/server_cert.pem")
+		}
+		if *keyFile == "" {
+			*keyFile = data.Path("x509/server_key.pem")
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials: %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterRouteGuideServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
 }
